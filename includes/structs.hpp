6393688a970f07a41addf6a770b5773660015b05
pudry <pudry@student.42lausanne.ch>
2024-02-01 14:42:19 +0100

    add struct request

--- includes/structs.hpp
+++ includes/structs.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   structs.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvandenb <dvandenb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pudry <pudry@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 15:59:51 by dvandenb          #+#    #+#             */
-/*   Updated: 2024/02/01 13:23:24 by dvandenb         ###   ########.fr       */
+/*   Updated: 2024/02/01 14:12:07 by pudry            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,8 @@ struct HttpRequest
 	std::string path;
 	std::string body;
 	std::string	HostPort;
+	int			RequestLength; // total request length of download file
+	int			length; // actual request length
 	bool		HtmlFile;
 	bool		emptyPath;
 };
